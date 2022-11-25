1e00ae67a3227c613ed471fea83153761dc8b005
mohmedMosad <117770596+mohmedMosad@users.noreply.github.com>
2022-11-26 01:53:24 +0200

    Update mohmed.java

--- mohmed.java
+++ mohmed.java
@@ -1,10 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
 
-public class Task_os {
 
-
+public class NewClass {
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("mohmed sabry");
-    }
+        System.out.println("mohmed mossad");
 
+    }
 }
